f81da516cb40d7e7a1982b1cc22053b28f326bd4
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-20 08:12:52 +0100

    add avatar api from multivatar for users, update logic to select avatar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Chat from './Pages/Chat';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import SetAvatar from './Pages/SetAvatar';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Chat />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/setavatar' element={<SetAvatar />} />
         <Route path='/login' element={<Login />} />
         <Route path='/chat' element={<Chat />} />
       </Routes>
